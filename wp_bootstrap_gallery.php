67a474adb68ab56fe412ab67cd4cf9a98dfc686a
Kees de Bruin <halfje.bruin@gmail.com>
2014-04-16 11:02:51 +0200

    Changed .span to .col-md- for Bootstrap 3

--- wp_bootstrap_gallery.php
+++ wp_bootstrap_gallery.php
@@ -151,7 +151,7 @@ function wp_bootstrap_gallery( $content, $attr ) {
 		$attachment_image = wp_get_attachment_image( $id, 'full');
 		$attachment_link = wp_get_attachment_link( $id, 'full', ! ( isset( $attr['link'] ) AND 'file' == $attr['link'] ) );
 		
-		$output .= "<div class='span" . $span_array[$attachment_count] . "'>";
+		$output .= "<div class='col-md-" . $span_array[$attachment_count] . "'>";
 		$output .= $attachment_link . "\n";
 		$output .= "</div>\n";
 
